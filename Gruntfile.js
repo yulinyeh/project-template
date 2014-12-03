58133165fa94032eea80a9c7cf62d653cb4b9db6
edenyeh <caco99@gmail.com>
2014-12-03 11:14:02 +0800

    新增常見 favicon; 將環境分為開發與產品

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    timestamp: '<%= new Date().getTime() / 1000000 >> 0 %>',
     jade: {
       dev: {
         options: {
           data: {
-            debug: false
+            debug: true
           },
           pretty: true
         },
-        // files: {
-        //   "Public/index.html": "Jade/partial/index.jade"
-        // }
         files: [{
           expand: true,
           cwd: 'Jade/partial',
@@ -19,6 +17,31 @@ module.exports = function(grunt) {
           dest: 'Public',
           ext: '.html'
         }]
+      },
+      prod: {
+        options: {
+          data: {
+            debug: false,
+            timestamp: '<%= timestamp %>'
+          },
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'Jade/partial',
+          src: ['*.jade'],
+          dest: 'Production',
+          ext: '.html'
+        }]
+      },
+      prod: {
+        options: {
+          noLineComments: true,
+          outputStyle: 'compressed',
+          sassDir: 'Sass',
+          cssDir: 'Temp',
+          environment: 'production'
+        }
       }
     },
     compass: {
@@ -28,68 +51,136 @@ module.exports = function(grunt) {
           noLineComments: true,
           outputStyle: 'expanded',
           sassDir: 'Sass',
-          cssDir: 'Public/assets/stylesheets',
+          cssDir: 'Development/assets/stylesheets',
           environment: 'development'
+        }
+      },
+      prod: {
+        options: {
+          noLineComments: true,
+          outputStyle: 'compressed',
+          sassDir: 'Sass',
+          cssDir: 'Temp',
+          environment: 'production'
         }
       }
     },
     concat: {
-      dist: {
+      dev: {
         src: [
-          'Javascript/main.js'
+          'Javascript/main.js',
         ],
-        dest: 'Public/assets/javascripts/script.js',
+        dest: 'Development/assets/javascripts/script.js'
+      },
+      prod: {
+        files: {
+          'Temp/script<%= timestamp %>.min.js': [
+            'Development/assets/javascripts/underscore-1.7.0.min.js',
+            'Development/assets/javascripts/jquery-1.11.1.min.js',
+            'Development/assets/javascripts/script.js'
+          ],
+          'Temp/style<%= timestamp %>.min.css': [
+            'Development/assets/stylesheets/semantic.min.css',
+            'Temp/main.css'
+          ],
+          'Temp/hack-ie8<%= timestamp %>.min.css': ['Temp/hack-ie8.css']
+        }
       }
     },
     uglify: {
-      my_target: {
+      prod: {
         files: {
-          'Public/assets/javascripts/script.min.js': ['Javascript/main.js']
+          'Temp/script.min.js': ['Development/assets/javascripts/script.js']
         }
       }
     },
     connect: {
-      server: {
+      dev: {
         options: {
           hostname: '0.0.0.0',
           port: 8000,
           base: './',
           livereload: true
         }
+      },
+      prod: {
+        options: {
+          hostname: '0.0.0.0',
+          port: 8000,
+          base: './',
+          livereload: false,
+          keepalive: true
+        }
       }
     },
     watch: {
       jade: {
         files: ['Jade/**/*.jade'],
-        tasks: ['jade']
+        tasks: ['jade:dev']
       },
       sass: {
         files: ['Sass/**/*.sass'],
-        tasks: ['compass']
+        tasks: ['compass:dev']
       },
       javascript: {
-        files: ['Javascript/**/*.js'],
-        tasks: ['concat']
+        files: ['Javascript/*.js'],
+        tasks: ['concat:dev']
       },
-      // javascript: {
-      //   files: ['Javascript/**/*.js'],
-      //   tasks: ['uglify']
-      // },
       livereload: {
         options: {
           livereload: true
         },
-        files: ['Public/**/*']
+        files: ['Development/**/*']
       },
+    },
+    clean: {
+      prod: ['Temp', 'Production']
+    },
+    copy: {
+      prod: {
+        files: [{
+          expand: true,
+          cwd: 'Development/assets/fonts/',
+          dest: 'Production/assets/fonts/',
+          src: ['**']
+        }, {
+          expand: true,
+          cwd: 'Development/assets/images/',
+          dest: 'Production/assets/images/',
+          src: ['**']
+        }, {
+          expand: true,
+          cwd: 'Development/assets/javascripts/',
+          dest: 'Production/assets/javascripts/',
+          src: ['html5shiv-printshiv.min.js']
+        }, {
+          expand: true,
+          cwd: 'Temp/',
+          dest: 'Production/assets/javascripts/',
+          src: ['script<%= timestamp %>.min.js']
+        }, {
+          expand: true,
+          cwd: 'Temp/',
+          dest: 'Production/assets/stylesheets',
+          src: ['style<%= timestamp %>.min.css']
+        }, {
+          expand: true,
+          cwd: 'Development/',
+          dest: 'Production/',
+          src: ['*.txt', 'favicon.ico']
+        }]
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['jade', 'compass', 'concat', 'connect', 'watch']);
-  // grunt.registerTask('default', ['jade', 'compass', 'uglify', 'connect', 'watch']);
+  grunt.registerTask('default', ['jade:dev', 'compass:dev', 'concat:dev', 'connect:dev', 'watch']);
+  grunt.registerTask('prod', ['clean:prod', 'jade:prod', 'compass:prod', 'uglify:prod', 'concat:prod', 'copy:prod', 'connect:prod']);
 };
