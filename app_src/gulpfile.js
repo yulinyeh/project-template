4a928d21240c801815dcd78321c49a1bd5a400e9
葉宇霖 <yehyulin@Yeh-Yulins-MacBook-Pro.local>
2015-09-08 17:24:58 +0800

    設定微調

--- app_src/gulpfile.js
+++ app_src/gulpfile.js
@@ -20,29 +20,39 @@ var rebuildNumber = 0;
 var host = 'http://edenyeh.ngrok.io/'; // 測試用
 var appId = '438786936303896'; // 測試用
 
-var filesSass = ['Sass/body.sass',
-    'Sass/component.sass'], // Sass 檔案
-  filesCSS = filesSass.map(function(file){return file.replace('Sass', 'tmp').replace('.sass', '.css');}),
-  filesComponentCSS = [], // Component 的 CSS
-  filesComponentFont = [], // Component 的 Font
+var filesSass = [
+    'sass/*.sass'], // Sass 檔案
+  filesCSS = filesSass.map(function(file){return file.replace('sass', 'tmp').replace('.sass', '.css');}),
+  filesComponentCSS = [], // Component 的 CSS(ex: 'components/fontawesome/css/font-awesome.min.css')
+  filesComponentFont = [], // Component 的 Font(ex: 'components/fontawesome/**/fonts/*.*')
   filesCSSMinify = filesComponentCSS.concat(filesCSS),
   filesJavascript = [
-    'Javascript/common.js'], // 自己寫的 JavaScript
+    'javascript/common.js'], // 自己寫的 JavaScript
   filesComponentJavascript = [
     'components/jquery/dist/jquery.min.js'], // Component 的 JavaScript
   filesComponentJavascriptMap = [
     'components/jquery/dist/jquery.min.map'], // Component 的 JavaScript Map
   filesJavascriptMinify = filesComponentJavascript.concat(['tmp/script.uglify.js']),
-  filesAssets = ['humans.txt', 'robots.txt', 'images/**/*.*', 'plugins/**/*.*'], // 純粹複製
-  fileHtml5shiv = ['components/html5shiv/dist/html5shiv-printshiv.min.js'], // 舊瀏覽器支援 HTML5 Tag
-  fileHtml5shiv_prod = ['javascripts/html5shiv-printshiv.min.js']; // 舊瀏覽器支援 HTML5 Tag (Production 路徑)
+  filesAssets = [
+    'images/**/*.*',
+    'plugins/**/*.*'], // 純粹複製
+  filesRootAssets = [
+    'humans.txt',
+    'robots.txt',
+    'favicon.ico',
+    'favicon.png',
+  ], // 純粹複製
+  fileHtml5shiv = [
+    'components/html5shiv/dist/html5shiv-printshiv.min.js'], // 舊瀏覽器支援 HTML5 Tag
+  fileHtml5shiv_prod = [
+    'javascripts/html5shiv-printshiv.min.js']; // 舊瀏覽器支援 HTML5 Tag (Production 路徑)
 
 // =============== 整體自動化 Start ===============
-gulp.task('html:init', ['clean:dev'], function() {
-  jade2html('./Jade/partial/**/*.jade');
+gulp.task('html:init', function() {
+  jade2html('./jade/partial/**/*.jade');
 });
 gulp.task('html:pretty', ['clean:prod'], function() {
-  gulp.src('./Jade/partial/**/*.jade')
+  gulp.src('./jade/partial/**/*.jade')
     .pipe(jade({
       locals: {
         host: host,
@@ -69,7 +79,8 @@ gulp.task('sass:rebuild', function() {
   return sass2css(filesSass);
 });
 gulp.task('sass:compressed', function() {
-  return gulp.src('./Sass/*.sass')
+  return gulp.src(filesSass)
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(sass({
       outputStyle: 'compressed',
       sourceMap: false
@@ -83,7 +94,7 @@ gulp.task('sass:compressed', function() {
 
 // Concat 串連
 // CSS
-gulp.task('css:init', ['clean:dev', 'sass:init'], function() {
+gulp.task('css:init', ['sass:init'], function() {
   return concat2style();
 });
 gulp.task('css:rebuild', ['sass:rebuild'], function() {
@@ -102,7 +113,7 @@ gulp.task('css:changed', function() {
   };
 });
 // JS
-gulp.task('js', ['clean:dev'], function() {
+gulp.task('js', function() {
   gulp.src(filesJavascript)
     .pipe(concat('script.js'))
     .pipe(gulp.dest('../app_dev/assets/javascripts/'))
@@ -132,7 +143,9 @@ gulp.task('concat:css-minify', ['clean:prod', 'sass:compressed'], function() {
 });
 gulp.task('concat:js-minify', ['clean:prod', 'uglify'], function() {
   gulp.src(filesJavascriptMinify)
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(concat('script.min.js'))
+    // .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('../app_prod/assets/javascripts/'))
     .pipe(gutil.buffer(function(err, files) {
       gutil.log(gutil.colors.yellow('concat:js-minify @ ' + new Date()));
@@ -153,42 +166,56 @@ gulp.task('uglify', function() {
 });
 
 // 複製 Components
-gulp.task('copy:component-js', ['clean:dev'], function() {
-  return gulp.src(filesComponentJavascript.concat(filesComponentJavascriptMap).concat(fileHtml5shiv))
+gulp.task('copy:components', function() {
+  return gulp.src(filesComponentJavascript.concat(filesComponentJavascriptMap).concat(filesComponentCSS).concat(filesComponentFont))
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(copy('../app_dev/assets/'))
     .pipe(gutil.buffer(function(err, files) {
-      gutil.log(gutil.colors.yellow('copy:component-js @ ' + new Date()));
+      gutil.log(gutil.colors.yellow('copy:components @ ' + new Date()));
     }));
 });
-gulp.task('copy:component-js-prod', ['clean:prod'], function() {
-  return gulp.src(filesComponentJavascriptMap.concat(fileHtml5shiv))
-    .pipe(flatten())
-    .pipe(gulp.dest('../app_prod/assets/javascripts/'))
+gulp.task('copy:components-prod-font', ['clean:prod'], function() {
+  return gulp.src(filesComponentFont)
+    .pipe(flatten({includeParents: 1}))
+    .pipe(gulp.dest('../app_prod/assets/'))
     .pipe(gutil.buffer(function(err, files) {
-      gutil.log(gutil.colors.yellow('copy:component-js @ ' + new Date()));
+      gutil.log(gutil.colors.yellow('copy:components-prod-font @ ' + new Date()));
     }));
 });
 
 // 複製 Images...
-gulp.task('copy:assets-dev', ['clean:dev'], function() {
-  return gulp.src(filesAssets)
+gulp.task('copy:dev', function() {
+  return gulp.src(['images/**/*.*', 'fake_files/**/*.*'])
     .pipe(copy('../app_dev/assets/'))
     .pipe(gutil.buffer(function(err, files) {
-      gutil.log(gutil.colors.yellow('copy:assets @ ' + new Date()));
+      gutil.log(gutil.colors.yellow('copy:dev @ ' + new Date()));
+    }));
+});
+gulp.task('copy:assets-root', function() {
+  return gulp.src(filesRootAssets)
+    .pipe(copy('../app_dev/'))
+    .pipe(gutil.buffer(function(err, files) {
+      gutil.log(gutil.colors.yellow('copy:assets-root @ ' + new Date()));
+    }));
+});
+gulp.task('copy:assets-dev', function() {
+  return gulp.src(filesAssets.splice(filesAssets.indexOf('images/**/*.*'), 1))
+    .pipe(copy('../app_dev/assets/'))
+    .pipe(gutil.buffer(function(err, files) {
+      gutil.log(gutil.colors.yellow('copy:assets-dev @ ' + new Date()));
     }));
 });
 gulp.task('copy:assets-prod', ['clean:prod'], function() {
-  return gulp.src(filesAssets)
+  return gulp.src(filesAssets.concat(filesRootAssets))
     .pipe(copy('../app_prod/assets/'))
     .pipe(gutil.buffer(function(err, files) {
-      gutil.log(gutil.colors.yellow('copy:assets @ ' + new Date()));
+      gutil.log(gutil.colors.yellow('copy:assets-prod @ ' + new Date()));
     }));
 });
 
 // 移除資料夾
 gulp.task('clean:dev', function() {
-  return gulp.src('../app_dev/', {read: false})
+  return gulp.src(['./tmp/', '../app_dev/'], {read: false})
     .pipe(clean({force: true}));
 });
 gulp.task('clean:prod', function() {
@@ -217,18 +244,31 @@ function jade2html(param) {
         host: host,
         appId: appId,
         fileHtml5shiv: fileHtml5shiv,
+        filesComponentCSS: filesComponentCSS,
         filesComponentJavascript: filesComponentJavascript
       },
       pretty: true
     }))
     .pipe(gulp.dest('../app_dev/'))
-    .pipe(connect.reload())
-    .pipe(gutil.buffer(function(err, files) {
-      gutil.log(gutil.colors.yellow('jade2html @ ' + new Date()));
-    }));
+      .on('finish', function() {
+        gulp.src(param)
+          .pipe(connect.reload())
+          .pipe(gutil.buffer(function(err, files) {
+            gutil.log(gutil.colors.yellow('jade2html @ ' + new Date()));
+          }))
+      });
 };
 
 function sass2css(param) {
+  var index;
+  var newPath;
+  if (typeof param === 'object') {
+    newPath = param;
+  } else {
+    var index = param.indexOf('app_src/');
+    var newPath = param.substring(index).replace('app_src', '.').replace('/sass', '/sass/**/');
+  };
+
   return gulp.src(param)
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(sourcemaps.init())
@@ -259,7 +299,7 @@ function concat2style() {
 // =============== 轉譯函式 End ===============
 
 // 起動 Server
-gulp.task('connect:dev', ['clean:dev'], function() {
+gulp.task('connect:dev', function() {
   return connect.server({
     root: '../app_dev/',
     port: 8000,
@@ -276,24 +316,24 @@ gulp.task('connect:prod', ['clean:prod'], function() {
   });
 });
 
-gulp.task('default', ['copy:assets-dev', 'copy:component-js', 'html:init', 'css:init', 'js', 'connect:dev'], function() {
+gulp.task('default', ['copy:assets-root', 'copy:assets-dev', 'copy:components', 'html:init', 'css:init', 'js', 'connect:dev'], function() {
   gulp
-    .watch('./Jade/partial/*.jade')
+    .watch('./jade/partial/*.jade')
     .on('change', function(e) {
       jade2html(e.path);
     });
   gulp
-    .watch(['./Jade/layout.jade', './Jade/include/*.jade'])
+    .watch(['./jade/layout.jade', './jade/include/*.jade'])
     .on('change', function(e) {
-      jade2html('./Jade/partial/*.jade');
+      jade2html('./jade/partial/*.jade');
     });
-  gulp.watch('./Sass/*.sass')
+  gulp.watch(filesSass)
     .on('change', function(e) {
       sass2css(e.path);
     });
-  gulp.watch(['./Sass/require/*.sass', './Sass/include/*.sass'], ['css:rebuild']);
-  gulp.watch('./tmp/*.css', ['css:changed']);
-  gulp.watch('./Javascript/*.js', ['js:rebuild']);
+  gulp.watch(['./sass/require/*.sass', './sass/include/*.sass'], ['css:rebuild']);
+  gulp.watch('./tmp/**/*.css', ['css:changed']);
+  gulp.watch('./javascript/*.js', ['js:rebuild']);
 });
-gulp.task('prod', ['copy:assets-prod', 'copy:component-js-prod', 'html:pretty', 'concat:css-minify', 'concat:js-minify', 'connect:prod']);
+gulp.task('prod', ['copy:assets-prod', 'copy:components-prod-font', 'html:pretty', 'concat:css-minify', 'concat:js-minify', 'connect:prod']);
 // gulp.task('prod', ['usemin', 'connect']);
