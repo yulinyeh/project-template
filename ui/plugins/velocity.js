7618f31bd08944a15b23b4351d0afb13f7cafa6d
yulinyeh <caco99@gmail.com>
2017-11-28 11:39:10 +0800

    plugin 判斷修改

--- ui/plugins/velocity.js
+++ ui/plugins/velocity.js
@@ -1,4 +1,8 @@
 import Velocity from 'velocity-animate'
-if (!process.server || process.static) {
-  window.Velocity = window.velocity = Velocity
-}
+
+// console.log(process.server)
+// console.log(process.browser)
+// console.log(process.static)
+// console.log(process.env.NODE_ENV)
+// nuxt.config.js 中，將此 plugin 設定為 `ssr: false` 的話，大致上不需要再另外判斷使用時機，就是只在 client 端執行
+window.Velocity = window.velocity = Velocity
