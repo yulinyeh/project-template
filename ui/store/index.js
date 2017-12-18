0aa383cbafd6306b9f5c63db8d7945559cd61d46
yulinyeh <caco99@gmail.com>
2017-12-18 20:15:07 +0800

    補充說明

--- ui/store/index.js
+++ ui/store/index.js
@@ -9,10 +9,10 @@ const createStore = () => {
       nuxtServerInit ({ commit }, { req }) {
         // nuxtServerInit is called by Nuxt.js before server-rendering every page
         // nuxtServerInit: 每頁開始前都來看看這裡，可回饋 $store
-        // middleware: 每頁可先行判斷的邏輯，可在裡面下載資料後再做事
-        // fetch: 下載資料回饋給 $store
-        // asyncData: 下載資料回饋給 data
-        // 資料處理完之後，才開始處理 view(template) 的相關資料
+        // middleware: 每頁可先行判斷的邏輯，可在裡面下載資料後再做事 (route 切換的時候不會執行)
+        // fetch: 下載資料回饋給 $store (route 切換的時候會再來執行一次)
+        // asyncData: 下載資料回饋給 data (route 切換的時候會再來執行一次)
+        // 資料處理完之後，才開始處理 view (template) 的相關資料
       }
     }
   })
