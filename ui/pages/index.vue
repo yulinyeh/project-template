f417d3d0b5f9ea3af4bfece0a1679838c2adb730
yulinyeh <caco99@gmail.com>
2017-12-25 18:24:41 +0800

    又再補充一下 computed 的用法

--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -23,8 +23,16 @@
       return {}
     },
     // computed: {
-    //   // query () { return this.$route.query } // 一層不能使用在 v-model 中，當使用者更新資料的時候，會出現 no setter 的錯誤訊息
-    //   // query () { return { order: this.$route.query } } // 如果只想要一開始有自己邏輯的初始值，又不想要受限要另外寫 set/get 的話，可以多包一層 object 去避開
+    //   // query () {
+    //   //   // 一層不能使用在 v-model 中，當使用者更新資料的時候，會出現 no setter 的錯誤訊息
+    //   //   return this.$route.query
+    //   // }
+    //   // query () {
+    //   //   // 如果只想要一開始有自己邏輯的初始值，又不想要受限要另外寫 set/get 的話，可以多包一層 object 去避開，但這樣其實也與 v-model 脫鉤了
+    //   //   // 目前是找到的應用場景是下拉選單會隨著 $route 跟著異動，所以需要初始值先依照 $route，之後再靠 @change 事件去更新 $route，這樣 query 會也連動著更新了
+    //   //   // 還不是因為 watch $route 一開始不會被觸發，要變動一次才會
+    //   //   return { order: this.$route.query }
+    //   // }
     // },
     head () {
       return {
