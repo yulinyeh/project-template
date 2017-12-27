86904b6dc415e2bf1ae30f0dc3bc95398ae8f0d0
yulinyeh <caco99@gmail.com>
2017-12-27 09:10:08 +0800

    擴充 plugins 的寫法

--- ui/plugins/velocity.js
+++ ui/plugins/velocity.js
@@ -5,4 +5,6 @@ import Velocity from 'velocity-animate'
 // console.log(process.static)
 // console.log(process.env.NODE_ENV)
 // nuxt.config.js 中，將此 plugin 設定為 `ssr: false` 的話，大致上不需要再另外判斷使用時機，就是只在 client 端執行
-window.Velocity = window.velocity = Velocity
+export default ({ app, route }) => {
+  window.Velocity = window.velocity = Velocity
+}
