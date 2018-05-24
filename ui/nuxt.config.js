7967f5c43ff2d97c6d68b06bfd3d11ee539a4e2a
yulinyeh <caco99@gmail.com>
2018-05-24 14:53:27 +0800

    修改 favicon 設定路徑

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -44,8 +44,8 @@ module.exports = {
       prefix: 'og: http://ogp.me/ns#'
     },
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
-      { rel: 'icon', type: 'image/png', href: './favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: 'favicon.png' }
     ],
     meta: [
       { charset: 'utf-8' },
