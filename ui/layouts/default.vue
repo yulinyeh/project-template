51f066f01b4c79096a2cf710e154b4504419d8e7
yulinyeh <caco99@gmail.com>
2019-01-23 11:16:20 +0800

    layout 模板骨架

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -1,12 +1,48 @@
 <template lang="pug">
-.layout-default
-  nuxt
+  .layout-default
+    .network-status(:class="{ 'is-offline': isOffline }")
+      nuxt
+      app-footer(:angel="{ name: 'Sachiel', color: '#0a3735' }")
+      .loading-mask(v-show="isDataLoading")
+      transition(name="v-delay")
+        toast-popup(key="toast-popup", v-if="toastPayload.description")
+      transition
+        dialog-popup(key="dialog-popup", v-if="(dialogPayload.title || dialogPayload.description) && typeof dialogPayload.confirmCallback === 'function'")
+    on-air-light(:class="{ 'is-offline': isOffline }")
 </template>
 
 <script>
-export default {
-  name: 'LayoutsDefault'
-}
+  import AppFooter from '../../../common-elements/components/app-footer.vue'
+  import DialogPopup from '../../../common-elements/components/popups/dialog-popup.vue'
+  import ToastPopup from '../../../common-elements/components/popups/toast-popup.vue'
+  import OnAirLight from '../../../common-elements/components/on-air-light.vue'
+  export default {
+    name: 'LayoutsDefault',
+    components: {
+      AppFooter,
+      DialogPopup,
+      ToastPopup,
+      OnAirLight
+    },
+    data () {
+      return {
+        ...require('../../../common-elements/assets/js/layouts/default/data'),
+      }
+    },
+    beforeMount () {
+      require('../../../common-elements/assets/js/layouts/default/before-mount').init(this)
+      this.initToken()
+    },
+    mounted () {
+      require('../../../common-elements/assets/js/layouts/default/mounted').init(this)
+    },
+    destroyed () {
+      require('../../../common-elements/assets/js/layouts/default/destroyed').init(this)
+    },
+    methods: {
+      ...require('../../../common-elements/assets/js/layouts/default/methods')
+    }
+  }
 </script>
 
 <style lang="sass" scoped src="@/assets/sass/layouts/default.sass" />
